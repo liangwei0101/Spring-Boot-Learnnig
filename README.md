e6c66478ac9b829e09ecf9768bad4282bd8e1546
liangwei0101 <liangwei0101@outlook.com>
2019-02-18 20:04:17 +0800

    增加文件

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Spring-Boot-Learnnig
+	
+  Spring-Boot 学习
+
---
+++ Spring-boot-redis-mysql/src/main/java/com/liangwei/demo/model/Book.java
@@ -0,0 +1,36 @@
+package com.liangwei.demo.model;
+
+import java.io.Serializable;
+
+public class Book implements Serializable {
+
+    private int id;
+
+    private String name;
+
+    private float price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
